84e9bba872857f380673be1d782d5b9c4412da99
minchul <minchulking91@gmail.com>
2017-09-01 01:20:23 +0900

    migrate RxJava2 convert kotlin

--- app/src/main/java/com/nobrain/devfest/reactiveandroid/repository/observer/Notify.java
+++
@@ -1,9 +0,0 @@
-package com.nobrain.devfest.reactiveandroid.repository.observer;
-
-
-import rx.Observable;
-import rx.Subscription;
-
-public interface Notify<T> {
-    Subscription update(Observable<Result<T>> observable);
-}
--- app/src/main/java/com/nobrain/devfest/reactiveandroid/view/OnRecyclerItemClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.nobrain.devfest.reactiveandroid.view;
-
-
-import android.support.v7.widget.RecyclerView;
-
-public interface OnRecyclerItemClickListener {
-    void onItemClick(RecyclerView.Adapter adapter, int position);
-}
